a69f9991e3fa9296993a0c9bf170763c0b862520
KMConner <KMConner@users.noreply.github.com>
2019-05-17 14:26:03 +0900

    Create a solution

---
+++ SeikyoNotifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeikyoNotifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
